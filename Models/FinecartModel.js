f1bc1760ad4a4303f2ab4b047ece15e5da43c0f2
kuldeep571 <kuldeepvishwakarma3084@gmail.com>
2024-02-13 16:52:28 -0500

    update code
    update code

--- Models/FinecartModel.js
+++ Models/FinecartModel.js
@@ -3,50 +3,39 @@ const ProductSchema = mongoose.Schema({
 
     image: {
         type: String,
-        required: true,
     },
     video: {
         type: String,
     },
     image1: {
         type: String,
-        required: true,
     },
     image2: {
         type: String,
-        required: true,
     },
     image3: {
         type: String,
-        required: true,
     },
     image4: {
         type: String,
-        required: true,
     },
     name: {
         type: String,
-        required: true,
     },
     price: {
         type: String,
-        required: true,
     },
     brand: {
         type: String,
-        required: true,
     },
     description: {
         type: String,
-        required: true,
     },
     category: {
         type: String,
-        required: true,
     },
     rating: {
         type: String,
-        required: true,
     },
     modelName:{
         type: String,
