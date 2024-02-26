12e7ea0b83e30012a1373ed376ce2fe2a626c2d6
kuldeep571 <kuldeepvishwakarma3084@gmail.com>
2024-02-26 15:26:16 -0500

    update
    update

--- Models/NewPro1Model.js
+++ Models/NewPro1Model.js
@@ -3,87 +3,71 @@ const newproSchema = mongoose.Schema({
 
     image: {
         type: String,
-        required: true,
     },
     video: {
         type: String,
     },
     image1: {
         type: String,
-        required: true,
     },
     image2: {
         type: String,
-        required: true,
     },
     image3: {
         type: String,
-        required: true,
     },
     image4: {
         type: String,
-        required: true,
     },
     name: {
         type: String,
-        required: true,
     },
     price: {
         type: String,
-        required: true,
     },
     brand: {
         type: String,
-        required: true,
     },
     description: {
         type: String,
-        required: true,
     },
-    rating:{
+    rating: {
         type: String,
-        required:true,
     },
     category: {
         type: String,
-        required: true,
     },
     cuttedprice: {
         type: String,
-        required: true,
     },
-    modelName:{
-        type: String,
-        required:true,
-    },
-    style:{
-        type: String,
-        required:true,
-    },
-    colour:{
-        type: String,
-        required:true,
-    },
-    screenSize:{
-        type: String,
-        required:true,
-    },
-    about_this_item:{
+    modelName: {
         type: String,
     },
-    material:{
+    style: {
         type: String,
     },
-    weight:{
+    colour: {
         type: String,
     },
-    product_dimensions:{
+    screenSize: {
         type: String,
     },
-    country_of_origin:{
+    about_this_item: {
         type: String,
     },
-    special_feature:{
+    material: {
+        type: String,
+    },
+    weight: {
+        type: String,
+    },
+    product_dimensions: {
+        type: String,
+    },
+    country_of_origin: {
+        type: String,
+    },
+    special_feature: {
         type: String,
     }
 
